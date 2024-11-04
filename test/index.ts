60783ef09a97c56c5c8f7908950c23d9e08d14be
linyyyang <linyyyang@tencent.com>
2024-11-05 07:15:56 +0800

    test: test case

--- test/index.ts
+++ test/index.ts
@@ -3,7 +3,10 @@
  * @Usage: 
  * @Author: richen
  * @Date: 2024-11-04 22:06:10
- * @LastEditTime: 2024-11-04 22:06:28
+ * @LastEditTime: 2024-11-05 07:15:34
  * @License: BSD (3-Clause)
  * @Copyright (c): <richenlin(at)gmail.com>
  */
+export class TestClass {
+
+}
